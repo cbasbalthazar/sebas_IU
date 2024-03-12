30fca464faa2549641f2b0a9fee6c940fe758362
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-03-12 13:57:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ st.header ("Este es un espacio de aprendizaje sobre ciudades inteligentes")
 image = Image.open('Imagen.jpg')
 st.image(image, caption = 'buenos días')
 st.write("Enlace para mi sistema de internet de las cosas")
-st.write("Ingresa al link www.hola.com")
+st.write("Ingresa al link [link](https://demo.thingsboard.io/dashboards/e2eb9330-e0a1-11ee-bc03-55147b89654f))
 
