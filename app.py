bcb16b8e6a40c3892fc268755a85135500f20798
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-03-12 13:31:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ import streamlit as st
 from PIL import Image
 
 st.title ("Aplicación Inteligencia Urbana")
+st.header ("Este es un espacio de aprendizaje sobre ciudades inteligentes")
+
+image = Image.open('imagen.jpg')
+st.image(image, caption = 'buenos días')
 
 
